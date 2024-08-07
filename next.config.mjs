5b4d582ac6c313e927c76b953d224308180274fe
Andrea Bonarrigo <andrea.bonarrigo@icloud.com>
2024-08-07 16:03:17 +0200

    Final commit before first deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**'
+      }
+    ],
+  },
+};
+
+  
+
 
 export default nextConfig;
+
